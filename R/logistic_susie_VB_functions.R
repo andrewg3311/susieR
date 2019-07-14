29b869c1f15070fcdf8637091adf45944461a884
Andrew Goldstein <andrewgoldstein@uchicago.edu>
2019-07-14 13:21:58 -0500

    Adding logistic functions

---
+++ R/logistic_susie_VB_functions.R
@@ -0,0 +1,294 @@
+### VB LOWER BOUND SUSIE BELOW
+
+### NOTE: This is basic code, and I did not attempt to mirror the level of numerical sophistication in the susie functions
+### If this idea is worth pursuing further, then this code can be improved
+
+calc_Q = function(X, Sigma2, Mu, Alpha, Z, delta) {
+  # X is data matrix
+  # Sigma2[j, l] is the posterior variance for b_l when entry j is selected, p x L
+  # Mu[j, l] is the posterior mean for b_l when entry j is selected, p x L
+  # Alpha[j, l] is the posterior probability selecting entry j from b_l, p x L
+  # Z is matrix of covariates (e.g. column for intercept, top 10 PCs, etc)
+  # delta is current estimate for effects of Z variables
+  
+  ASU2 = Alpha * (Sigma2 + Mu^2) # [j, l] = alpha[j, l] * (Sigma2[j, l] + Mu[j, l]^2)
+  
+  Q = rowSums(X^2 %*% ASU2) # start w/ sum_l E_(ql)[(x_i' b_l)^2]
+  
+  # now add 2 sum_l sum_{k>l} (x_i' b_l_post)(x_i' b_k_post)
+  # maybe try to think of a more efficient way of doing this?
+  b_post_mat = Mu * Alpha # each column is posterior mean for b_l, p x L
+  X_b_post = X %*% b_post_mat # [i, l] = x_i' b_l_post
+  for (i in 1:nrow(X)) {
+    xb_i = as.numeric(X_b_post[i, ])
+    xb_outer = xb_i %o% xb_i # outer-product, [j, k] = (x_i'b_j_post)(x_i' b_k_post)
+    diag(xb_outer) = 0 # remove diagonal, where j = k
+    Q[i] = Q[i] + sum(xb_outer)
+  }
+  
+  # now, add other terms with Z and delta
+  Q = Q + as.numeric(2 * (X %*% rowSums(b_post_mat)) * (Z %*% delta)) + as.numeric((Z %*% delta)^2)
+  
+  return(Q)
+  
+}
+
+g = function(x) { # ilogit function
+  1 / (1 + exp(-x))
+}
+
+
+update_xi = function(X, Sigma2, Mu, Alpha, Z, delta) {
+  # X is data matrix
+  # Sigma2[j, l] is the posterior variance for b_l when entry j is selected, p x L
+  # Mu[j, l] is the posterior mean for b_l when entry j is selected, p x L
+  # Alpha[j, l] is the posterior probability selecting entry j from b_l, p x L
+  # Z is matrix of covariates (e.g. column for intercept, top 10 PCs, etc)
+  # delta is current estimate for effects of Z variables
+  
+  Q = calc_Q(X, Sigma2, Mu, Alpha, Z, delta)
+  
+  xi = sqrt(Q)
+  
+  return(xi)
+  
+}
+
+
+update_b_l = function(X, Y, xi, prior_weights, V, Sigma2, Mu, Alpha, l, Z, delta) {
+  # X is data matrix
+  # Y is binary response
+  # xi is lower-bound approximation parameters
+  # prior_weights is prior probabilities for selecting j (p-vector)
+  # V is prior variance of current effect (scalar)
+  # Sigma2[j, l] is the posterior variance for b_l when entry j is selected, p x L
+  # Mu[j, l] is the posterior mean for b_l when entry j is selected, p x L
+  # Alpha[j, l] is the posterior probability selecting entry j from b_l, p x L
+  # l is index to update
+  # Z is matrix of covariates (e.g. column for intercept, top 10 PCs, etc)
+  # delta is current estimate for effects of Z variables
+  
+  b_post_mat = Mu * Alpha # each column is posterior mean for b_l, p x L
+  b_post_not_l = rowSums(as.matrix(b_post_mat[, -l], nrow = nrow(Mu))) # posterior, sum_(k != l) b_k_post
+  g_xi = g(xi) # vector of g(xi_i), pre-compute once
+  g_xi_5_xi = ((g_xi - .5) / xi) # vector of (g(xi_i) - .5) / xi_i, pre-compute once
+  g_xi_5_xi[xi == 0] = .25 # case of 0/0 (e.g. x_i is all 0), use L'Hopital
+  
+  common_denoms = (1 / V) + (t(X^2) %*% g_xi_5_xi) # appears many times, compute once, posterior precisions
+  
+  # update Alpha[, l]
+  nums = t(X) %*% (Y - .5 -  (g_xi_5_xi * ((X %*% b_post_not_l) + (Z %*% delta)))) # numerator in exp
+  Alpha[, l] = as.numeric(log(prior_weights) + (nums^2 / (2*common_denoms)) - (1/2)*log(common_denoms))
+  Alpha[, l] = exp(Alpha[, l] - max(Alpha[, l])) # remove max for stability, everything still proportional
+  Alpha[, l] = Alpha[, l] / sum(Alpha[, l]) # normalize, sum to 1
+  
+  # update Mu[, l]
+  Mu[, l] = as.numeric(nums / common_denoms)
+  
+  # update Sigma[, l]
+  Sigma2[, l] = as.numeric(1 / common_denoms)
+  
+  return(list(Sigma2 = Sigma2, Mu = Mu, Alpha = Alpha))
+  
+}
+
+
+update_delta = function(X, Y, xi, Mu, Alpha, Z) {
+  # X is data matrix
+  # Y is binary response
+  # xi is lower-bound approximation parameters
+  # Mu[j, l] is the posterior mean for b_l when entry j is selected, p x L
+  # Alpha[j, l] is the posterior probability selecting entry j from b_l, p x L
+  # Z is matrix of covariates (e.g. column for intercept, top 10 PCs, etc)
+  
+  g_xi = g(xi) # vector of g(xi_i), pre-compute once
+  g_xi_5_xi = ((g_xi - .5) / xi) # vector of (g(xi_i) - .5) / xi_i, pre-compute once
+  g_xi_5_xi[xi == 0] = .25
+  
+  #D = Matrix::Diagonal(x = g_xi_5_xi / 2)
+  D = Matrix::Diagonal(x = g_xi_5_xi)
+  ZtDZ = t(Z) %*% D %*% Z
+  DXB = D %*% X %*% rowSums(Mu * Alpha)
+  RHS = t(Z) %*% (Y - .5 - DXB) # RHS of system LL'delta = RHS
+  
+  # NOTE: the below system can/should be solved w/ Cholesky and forward/backward substitution (but if Z has small # of columns, shouldn't matter)
+  #Lt = chol(ZtDZ)
+  #u = forwardsolve(t(Lt), RHS)
+  #delta = backsolve(Lt, u)
+  ## COULD USE chol2inv!!!!
+  #delta = solve(ZtDZ, RHS)
+  delta = solve(as.matrix(ZtDZ), as.matrix(RHS))
+  
+  return(as.numeric(delta))
+  
+}
+
+update_all = function(X, Y, xi, prior_weights, V, Sigma2, Mu, Alpha, Z, delta, estimate_prior_variance) {
+  # X is data matrix
+  # Y is binary response
+  # xi is lower-bound approximation parameters
+  # prior_weights is prior probabilities for selecting j (p-vector)
+  # V is prior variance (scalar if the same for all L effects, or vector of length L if different)
+  # Sigma2[j, l] is the posterior variance for b_l when entry j is selected, p x L
+  # Mu[j, l] is the posterior mean for b_l when entry j is selected, p x L
+  # Alpha[j, l] is the posterior probability selecting entry j from b_l, p x L
+  # Z is matrix of covariates (e.g. column for intercept, top 10 PCs, etc)
+  # delta is current estimate for effects of Z variables
+  # estimate_prior_variance is logical for if prior variance, V, should be estimated
+  
+  L = ncol(Mu)
+  
+  # update delta
+  if (any(Z != 0)) { # if covariates and/or intercept
+    delta = update_delta(X, Y, xi, Mu, Alpha, Z)
+  }
+  
+  if (length(V) == L) {
+    prior_vars = V
+  } else if (length(V) == 1) {
+    prior_vars = rep(V, L)
+  } else {
+    stop("Argument 'V' must be of length either 1 or L")
+  }
+  
+  # now, iterate over l = 1:L
+  for (l in 1:L) {
+    res_l = update_b_l(X, Y, xi, prior_weights, prior_vars[l], Sigma2, Mu, Alpha, l, Z, delta)
+    Sigma2 = res_l$Sigma2
+    Mu = res_l$Mu
+    Alpha = res_l$Alpha
+  }
+  
+  # now, update xi
+  xi = update_xi(X, Sigma2, Mu, Alpha, Z, delta)
+  
+  if (estimate_prior_variance == TRUE) { # if estimating prior variance
+    ASU2 = Alpha * (Sigma2 + Mu^2) # [j, l] = alpha[j, l] * (Sigma2[j, l] + Mu[j, l]^2)
+    if (length(V) == 1) { # if common prior variance for all effects
+      V = sum(ASU2) / ncol(Mu) # ncol(Mu) = L = sum_l sum_j alpha_jl
+    } else { # if different for all effects (already know length is L, checked a few lines before)
+      V = colSums(ASU2)
+    }
+  }
+
+  
+  return(list(Sigma2 = Sigma2, Mu = Mu, Alpha = Alpha, delta = delta, xi = xi, V = V))
+  
+}
+
+# Y is vector of binary response (n x 1, can be sparse)
+# X is matrix of variables (n x p, can be sparse)
+# L is number of non-zero effects, positive integer
+# V is prior variance on non-zero effect size, scalar or L-vector
+# prior_weights is a vector of prior inclusion probabilities (p x 1)
+# intercept is a logical of if the intercept should be fitted (default to TRUE)
+# Z is a vector of covariates to be controlled for (non-penalized effect estimates, n x q, can be sparse).
+# NOTE: Z should NOT include an intercept column
+# estimate_prior_variance is a logical if prior variance, V, should be estimated. If "TRUE", supplied value of V is the initial value
+# tol is the convergence criterior measuring the change in the ELBO
+# maxit is the maximum number of iterations
+susie_logistic_VB = function(Y, X, L = 10, V = 1, prior_weights = NULL, intercept = TRUE, Z = NULL, estimate_prior_variance = FALSE, tol = 1e-3, maxit = 1000) {
+  p = ncol(X)
+  n = nrow(X)
+  
+  if (is.null(Z)) {
+    if (intercept == TRUE) {
+      Z = matrix(1, nrow = n, ncol = 1)
+    } else {
+      Z = matrix(0, nrow = n, ncol = 1) # if no intercept and no control covariates, set to 0
+    }
+  } else {
+    col_variances = apply(Z, MARGIN = 2, var)
+    if (any(col_variances == 0)) { # is constant column in Z matrix
+      stop("Matrix 'Z' cannot have a constant column")
+    }
+    Z = cbind(matrix(1, nrow = n, ncol = 1), Z) # add intercept column
+  }
+  
+  if (is.null(prior_weights)) {
+    prior_weights = rep(1 / p, p)
+  }
+  
+  # place to store posterior info for each l = 1, ..., L
+  # initialize: could think of something better
+  #delta = glm(Y ~ Z - 1, family = "binomial")$coef # initialize to regular GLM solution
+  if (all(Z == 0)) { # if no covariates and no intercept
+    delta = 0
+  } else {
+    delta = glm(as.numeric(Y) ~ Z - 1, family = "binomial")$coef # initialize to regular GLM solution w/ just Z (no X)
+  }
+  Alpha = matrix(prior_weights, nrow = p, ncol = L)
+  #Alpha = t(MCMCpack::rdirichlet(L, prior_weights)) # alternate initialization method
+  Mu = matrix(0, nrow = p, ncol = L)
+  Sigma2 = matrix(V, nrow = p, ncol = L, byrow = T)
+  xi = update_xi(X, Sigma2, Mu, Alpha, Z, delta)
+  post_info = list(Sigma2 = Sigma2, Mu = Mu, Alpha = Alpha, delta = delta, xi = xi, V = V)
+  
+  #beta_post_init = matrix(Inf, nrow = p, ncol = L) # initialize
+  #beta_post_init2 = beta_post_init
+  beta_post = post_info$Alpha * post_info$Mu
+  
+  ELBOs = numeric(maxit + 1)
+  ELBOs[1] = -Inf
+  ELBOs[2] = calc_ELBO(Y, X, post_info$Alpha, post_info$Mu, post_info$Sigma2, post_info$V, prior_weights, Z, post_info$delta, post_info$xi)
+  
+  iter = 1
+  #while((norm(beta_post - beta_post_init, "1") > tol) & (norm(beta_post - beta_post_init2, "1") > tol)) { # repeat until posterior means
+  while(abs(ELBOs[iter+1] - ELBOs[iter]) > tol) { # repeat until ELBO increase is negligible
+    post_info = update_all(X, Y, post_info$xi, prior_weights, post_info$V, post_info$Sigma2, post_info$Mu, post_info$Alpha, Z, post_info$delta, estimate_prior_variance)
+    
+    beta_post = post_info$Alpha * post_info$Mu
+    
+    iter = iter + 1
+    ELBOs[iter + 1] = calc_ELBO(Y, X, post_info$Alpha, post_info$Mu, post_info$Sigma2, post_info$V, prior_weights, Z, post_info$delta, post_info$xi)
+    if (iter > maxit) {
+      stop("Maximum number of iterations reached")
+    }
+  }
+  
+  # change output format to match the GLM version's output
+  int = NULL
+  delta = post_info$delta
+  if (intercept == TRUE) {
+    int = post_info$delta[1]
+    delta = post_info$delta[-1]
+  }
+  if (length(delta) == 0) {
+    delta = 0
+  }
+  res = list(post_alpha = post_info$Alpha, post_mu = post_info$Mu, post_sigma = post_info$Sigma2, intercept = int, delta = delta, xi = post_info$xi, ELBOs = ELBOs[2:(iter+1)], prior_variance = post_info$V)
+  
+  return(res)
+  
+}
+
+
+# calculate the variational lower bound
+# CAREFUL: Not sure what to do when Alpha[j, l] = 0 (we get 0*ln(0)). I will set this to 0
+# Note: This expression is only valid when xi has been updated to be sqrt(Q), where Q is the 
+# expectation of the square of the linear predictor under the variational posterior (what we update xi to nomrally)
+calc_ELBO = function(Y, X, Alpha, Mu, Sigma2, V, prior_weights, Z, delta, xi) {
+  p = nrow(Mu)
+  L = ncol(Mu)
+  P = matrix(prior_weights, nrow = p, ncol = L)
+  b_post = rowSums(Alpha * Mu)
+  
+  V = matrix(V, nrow = p, ncol = L, byrow = T) # for proper arithmetic, either if V is a scalar or a vector of length L
+  
+  expected_log_lik = sum(log(g(xi)) + (Y - .5) * as.numeric(((X %*% b_post) + (Z %*% delta))) - (xi / 2))
+  KL_div_vb_prior = Alpha * (log(Alpha) - log(P) + (log(V) / 2) - (log(Sigma2) / 2) - .5 + ((Sigma2 + Mu^2) / (2 * V)))
+  KL_div_vb_prior[Alpha == 0] = 0
+  KL_div_vb_prior = sum(KL_div_vb_prior)
+  
+  if (KL_div_vb_prior < 0) { # to diagnose any issues
+    warning("KL Divergence calculated to be < 0")
+  }
+  
+  # TERMS BELOW WRONG B/C FORGOT MULTIPLICIATIVE FACTOR OF alpha_jl
+  #KL_div_vb_prior = sum(log(Alpha)) - L*sum(log(prior_weights)) + (L*p*log(V) / 2) - sum(log(Sigma2) / 2) - (L*p / 2) + (sum(Sigma2 + Mu^2) / (2*V))
+  #KL_div_vb_prior = sum(log(Alpha)) - sum(log(Sigma2) / 2) + (sum(Sigma2 + Mu^2) / (2*V)) # up to a constant
+  
+  ELBO = expected_log_lik - KL_div_vb_prior
+  return(ELBO)
+}
+
--- R/susie_logistic.R
+++ R/susie_logistic.R
@@ -1,101 +1,169 @@
-### NOTE: This is basic code, and I did not attempt to mirror the level of numerical sophistication in the susie functions
-### If this idea is worth pursuing further, then this code can be improved
-
-
-
-susie_logistic = function(Y, X, L = 10, V = 1, prior_weights = NULL, null_weight=NULL, 
-                          standardize = TRUE, coverage = 0.95, min_abs_corr = 0.5, max_iter = 100, tol = 1e-3) {
+#' @title SUm of Single Effect (SuSiE) Logistic Regression of Y on X
+#' @details Performs Bayesian logistic regression of Y on X.
+#' That is, this function
+#' fits the regression model Y ~ Bern(sum_l Xb_l), where
+#' sum_l b_l is a p vector of effects to be estimated.
+#' The assumption is that each b_l has exactly one non-zero element, with all elements
+#' equally likely to be non-zero. The prior on the non-zero element is N(0, var = prior_variance).
+#' @param X an n by p matrix of covariates
+#' @param Y an n vector (binary)
+#' @param L maximum number of non-zero effects
+#' @param prior_variance the prior variance (vector of length L, or scalar. In latter case gets repeated L times). The prior variance on each non-zero element of b is set to be prior_variance.
+#' @param prior_weights a p vector of prior probability that each element is non-zero
+#' @param intercept Should intercept be fitted (default=TRUE) or set to zero (FALSE). The latter is generally not recommended.
+#' @param Z an n by q vector of covariates to include in the model (e.g. top 10 PCs, gender, etc). The estimated effects
+#' for these covarites are not penalized, and there is no prior on the effects, the algorithm simply maximizes the ELBO w.r.t. the effects.
+#' Do not include a constant column for the intercept, this is handled separately.
+#' @param estimate_prior_variance indicates whether to estimate prior (initial estimate given by prior_variance. If prior_variance is
+#' a vector, then a different prior variance is estimated for each of the L effects)
+#' @param s_init a previous susie fit with which to initialize (NOT CURRENTLY IMPLEMENTD IN LOGISTIC VERSION)
+#' @param coverage coverage of confident sets. Default to 0.95 for 95\% credible interval.
+#' @param min_abs_corr minimum of absolute value of correlation allowed in a credible set.
+#' Default set to 0.5 to correspond to squared correlation of 0.25,
+#' a commonly used threshold for genotype data in genetics studies.
+#' @param max_iter maximum number of iterations to perform
+#' @param tol convergence tolerance
+#' @param verbose if true outputs some progress messages
+#' @param track_fit add an attribute \code{trace} to output that saves current values of all iterations (NOT CURRENTLY IMPLEMENTD IN LOGISTIC VERSION)
+#' @return a susie fit, which is a list with some or all of the following elements\cr
+#' \item{alpha}{an L by p matrix of posterior inclusion probabilites}
+#' \item{mu}{an L by p matrix of posterior means (conditional on inclusion)}
+#' \item{mu2}{an L by p matrix of posterior second moments (conditional on inclusion)}
+#' \item{Xr}{an n vector of fitted values, equal to X times colSums(alpha*mu))}
+#' \item{sigma2}{residual variance}
+#' \item{V}{prior variance}
+#' \item{elbo}{a vector of values of elbo achieved (objective function)}
+#' \item{sets}{a list of `cs`, `purity` and selected `cs_index`}
+#' \item{pip}{a vector of posterior inclusion probability}
+#' \item{z}{a vector of univariate z-scores}
+#' @examples
+#' set.seed(1)
+#' n = 1000
+#' p = 1000
+#' beta = rep(0,p)
+#' beta[1:4] = 1
+#' X = matrix(rnorm(n*p),nrow=n,ncol=p)
+#' y = rbinom(1000, 1, 1 / (1 + exp(-X %*% beta)))
+#' res = susie_logistic(X,y,L=10)
+#' coef(res)
+#' plot(y,predict(res))
+#'
+#' @importFrom stats var
+#' @importFrom utils modifyList
+#'
+#' @export
+#'
+susie_logistic = function(X, Y, L=  10, prior_variance = 1, prior_weights = NULL, 
+                 intercept = TRUE, Z = NULL, estimate_prior_variance = FALSE,
+                 s_init = NULL, coverage=0.95, min_abs_corr=0.5,
+                 max_iter = 100, tol = 1e-3, verbose = FALSE, track_fit = FALSE) {
+  
+  
+  # Check input X.
+  if (!(is.double(X) & is.matrix(X)) & !inherits(X,"CsparseMatrix"))
+    stop("Input X must be a double-precision matrix, or a sparse matrix.")
   
   p = ncol(X)
   n = nrow(X)
   
-  # place to store posterior info for each l = 1, ..., L
-  post_alpha = matrix(NA, nrow = p, ncol = L)
-  post_mu = matrix(NA, nrow = p, ncol = L)
-  post_sigma = matrix(NA, nrow = p, ncol = L)
-  post_info = list(post_alpha = post_alpha, post_mu = post_mu, post_sigma = post_sigma)
-
-  beta_post_init = matrix(Inf, nrow = p, ncol = L) # initialize
-  beta_post = matrix(0, nrow = p, ncol = L)
-  
-  while(norm(beta_post - beta_post_init, "1") > tol) { # repeat until posterior means converge (ELBO not calculated here, so use this convergence criterion instead)
-    beta_post_init = beta_post
-    
-    for (l in 1:L) {
-      
-      fixed = rowSums(X %*% beta_post[, -l]) # fixed portion from previous estimates
-      SER_logistric_l = single_effect_regression_logistic(Y, X, V, prior_weights, FALSE, fixed)
-      # store
-      post_info$post_alpha[, l] = SER_logistric_l$alpha
-      post_info$post_mu[, l] = SER_logistric_l$mu
-      post_info$post_sigma[, l] = SER_logistric_l$mu2 - SER_logistric_l$mu^2
-      
-      # update beta_post
-      beta_post[, l] = SER_logistric_l$alpha * SER_logistric_l$mu
-      
+  # Check Z
+  if (is.null(Z)) {
+    if (intercept == TRUE) {
+      Z = matrix(1, nrow = n, ncol = 1)
+    } else {
+      Z = matrix(0, nrow = n, ncol = 1) # if no intercept and no control covariates, set to 0
     }
-    
-  }
-  
-  return(post_info)
-  
-}
-
-
-# SER_logistic function
-single_effect_regression_logistic = function(Y, X, V, prior_weights = NULL, optimize_V = FALSE, fixed = NULL) {
-  p = ncol(X)
-  
-  betahat = numeric(p)
-  shat2 = numeric(p)
-  
-  if (is.null(fixed)) { # fixed is components from previous SER fits
-    fixed = rep(0, length(Y))
-  }
-  
-  # NOTE: could parallelize loop below if desired
-  for (j in 1:p) { # logistic regression on each column of X separately
-    log.fit = glm(Y ~ X[, j] - 1 + offset(fixed), family = "binomial") # fit w/out intercept
-    log.fit.coef = summary(log.fit)$coefficients
-    betahat[j] = ifelse(is.na(log.fit.coef[1]), 0, log.fit.coef[1]) # beta-hat MLE (if na, just set to 0)
-    shat2[j] = ifelse(is.na(log.fit.coef[2]), Inf, log.fit.coef[2]^2) # (std errof beta-hat MLE)^2 (if na, just set to Inf)
+  } else {
+    col_variances = apply(Z, MARGIN = 2, var)
+    if (any(col_variances == 0)) { # is constant column in Z matrix
+      stop("Matrix 'Z' cannot have a constant column")
+    }
+    Z = cbind(matrix(1, nrow = n, ncol = 1), Z) # add intercept column
   }
   
   if (is.null(prior_weights)) {
     prior_weights = rep(1 / p, p)
   }
   
-  if(optimize_V) {
-    stop("Optimizing for prior variance not yet implemented for logistic case")
-    #if(loglik.grad(0, betahat, shat2, prior_weights) < 0) {
-    #  V = 0
-    #} else {
-    ##V.o = optim(par=log(V),fn=negloglik.logscale,gr = negloglik.grad.logscale,betahat=betahat,shat2=shat2,prior_weights=prior_weights,method="BFGS")
-    ##if(V.o$convergence!=0){
-    ##  warning("optimization over prior variance failed to converge")
-    ##}
-    #  V.u = uniroot(negloglik.grad.logscale, c(-10, 10), extendInt = "upX", betahat = betahat, shat2 = shat2, prior_weights = prior_weights)
-    #  V = exp(V.u$root)
-    #}
+ 
+  # initialize: could think of something better
+  #delta = glm(Y ~ Z - 1, family = "binomial")$coef # initialize to regular GLM solution
+  if (all(Z == 0)) { # if no covariates and no intercept
+    delta = 0
+  } else {
+    delta = glm(as.numeric(Y) ~ Z - 1, family = "binomial")$coef # initialize to regular GLM solution w/ just Z (no X)
+  }
+  Alpha = matrix(prior_weights, nrow = p, ncol = L)
+  #Alpha = t(MCMCpack::rdirichlet(L, prior_weights)) # alternate initialization method
+  Mu = matrix(0, nrow = p, ncol = L)
+  Sigma2 = matrix(prior_variance, nrow = p, ncol = L, byrow = T)
+  xi = update_xi(X, Sigma2, Mu, Alpha, Z, delta)
+  post_info = list(Sigma2 = Sigma2, Mu = Mu, Alpha = Alpha, delta = delta, xi = xi, V = prior_variance)
+  
+  beta_post = post_info$Alpha * post_info$Mu
+  
+  elbo = numeric(max_iter + 1)
+  elbo[1] = -Inf
+  elbo[2] = calc_ELBO(Y, X, post_info$Alpha, post_info$Mu, post_info$Sigma2, post_info$V, prior_weights, Z, post_info$delta, post_info$xi)
+  
+  iter = 1
+  while(abs(elbo[iter+1] - elbo[iter]) > tol) { # repeat until ELBO increase is negligible
+    post_info = update_all(X, Y, post_info$xi, prior_weights, post_info$V, post_info$Sigma2, post_info$Mu, post_info$Alpha, Z, post_info$delta, estimate_prior_variance)
+    
+    iter = iter + 1
+    elbo[iter + 1] = calc_ELBO(Y, X, post_info$Alpha, post_info$Mu, post_info$Sigma2, post_info$V, prior_weights, Z, post_info$delta, post_info$xi)
+    
+    if(verbose){
+      print(paste("objective:", elbo[iter + 1]))
+    }
+    
+    if (iter > max_iter) {
+      stop("Maximum number of iterations reached")
+    }
   }
   
-  lbf = dnorm(betahat, 0, sqrt(V + shat2), log = TRUE) - dnorm(betahat, 0, sqrt(shat2), log = TRUE)
-  #log(bf) on each SNP
+  # change output format to match the GLM version's output
+  int = NULL
+  delta = post_info$delta
+  if (intercept == TRUE) {
+    int = post_info$delta[1]
+    delta = post_info$delta[-1]
+  }
+  if (length(delta) == 0) {
+    delta = 0
+  }
   
-  lbf[is.infinite(shat2)] = 0 # deal with special case of infinite shat2 (eg happens if X does not vary)
+  # change output to match linear version
+  s = list(alpha = t(post_info$Alpha), mu = t(post_info$Mu), mu2 = t(post_info$Sigma2 + post_info$Mu^2),
+           Xr = rep(NA, n), KL = rep(NA, L), lbf = rep(NA, L), sigma2 = NA, V = post_info$V, pi = prior_weights, null_index = 0)
+  class(s) = 'susie'
   
-  maxlbf = max(lbf)
-  w = exp(lbf - maxlbf) # w is proportional to BF, but subtract max for numerical stability
-  # posterior prob on each SNP
-  w_weighted = w * prior_weights
-  weighted_sum_w = sum(w_weighted)
-  alpha = w_weighted / weighted_sum_w
-  post_var = 1 / ((1 / shat2) + (1 / V)) # posterior variance
-  post_mean = (1 / shat2) * post_var * betahat # posterior mean
-  post_mean2 = post_var + post_mean^2 # posterior second moment
-  # BF for single effect model
-  lbf_model = maxlbf + log(weighted_sum_w)
-  # NOTE: Need to double check below
-  loglik = lbf_model + log(1/2)*length(Y) # loglik of 0/1 response Y under p = .5
-  return(list(alpha = alpha, mu = post_mean, mu2 = post_mean2, lbf = lbf, lbf_model = lbf_model, V = V, loglik = loglik))
+
+  elbo = elbo[2:(iter+1)] #remove trailing NAs and leading -Inf
+  s$elbo <- elbo
+  s$niter <- iter
+
+  if(intercept){
+    s$intercept = int
+  } else {
+    s$intercept = 0
+    s$fitted = s$Xr
+  }
+
+  s$delta = delta
+  
+  beta_post = colSums(s$alpha * s$mu)
+  lin_pred = (X %*% beta_post) + s$intercept
+  if (s$delta != 0) {
+    lin_pred = lin_pred + (Z %*% delta)
+  }
+  
+  s$fitted = 1 / (1 + exp(-lin_pred))
+
+  ## SuSiE CS and PIP
+  if (!is.null(coverage) && !is.null(min_abs_corr)) {
+    s$sets = susie_get_cs(s, coverage = coverage, X = X, min_abs_corr = min_abs_corr)
+    s$pip = susie_get_pip(s, s$sets$cs_index)
+  }
+  return(s)
 }
